f600e816b4c902e9f5b75c211e912e08b222a757
williamsca0901 <williamsca0901@gmail.com>
2022-01-30 22:02:42 -0500

    The basic structure of car is ready without many features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// the basic structure
         CarBasics car = new CarBasics();
-        car.color="green";
-        car.number="88877375";
-        car.engine="5444cc";
-        car.door="4";
-        System.out.println(car.color);
     }
 }
