09a0a383f709c6c3859e12ec17fdc52e754a4f1f
williamsca0901 <williamsca0901@gmail.com>
2022-01-30 22:39:16 -0500

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// the basic structure
         CarBasics car = new CarBasics();
+        car.color="red";
+        car.engine="7999ccc";
+        car.door="4";
+        car.number="1190189";
+        System.out.println(car.color);
     }
 }
